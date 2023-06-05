e2ed9911b168ff91e0e4aa726c53aab14ef859ff
Kondabala Bindhu <110176663+Bindhu09@users.noreply.github.com>
2023-06-05 21:40:46 +0530

    Perfomed first DOM Manipulation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("Hi I'm Bindhu")
+var data1={image:"Bindhu1.jpg",name:"Kondabala Bindhu",designation:"Intern"};
+var data2={image:"Bindhu2.jpg",name:"Indhu",designation:"software developer"};
+var isdata1=true;
+var displaydata;
+var changemypic=function(){
+    if(isdata1){
+        displaydata=data2;
+        isdata1=false
+    }
+    else{
+        displaydata=data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    
+}
